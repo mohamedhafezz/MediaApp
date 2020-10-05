85a622447084ad957d45380373781c1baf0ea472
Mohamed Hafez <m_hafez_95@hotmail.com>
2020-10-05 21:01:20 +0200

    Updates

---
+++ precache-manifest.af627e72f26b61c3f5e78efb4f6fa4e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c080b26d57584281705b36c852db16ff",
+    "url": "/MediaApp/index.html"
+  },
+  {
+    "revision": "86ba405f478d99e450e7",
+    "url": "/MediaApp/static/css/main.8c9b18f2.chunk.css"
+  },
+  {
+    "revision": "f65ead38bf480e6cfd30",
+    "url": "/MediaApp/static/js/2.322f2f7e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MediaApp/static/js/2.322f2f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ba405f478d99e450e7",
+    "url": "/MediaApp/static/js/main.8aecc396.chunk.js"
+  },
+  {
+    "revision": "336378e47b51252ff907",
+    "url": "/MediaApp/static/js/runtime-main.23e39763.js"
+  }
+]);
